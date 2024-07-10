df5c59b76bd45e44d510fbfb2f5a55934c11848a
lemosbor <68635720+lemosbor@users.noreply.github.com>
2024-07-10 23:48:50 +0300

    commit

---
+++ manual/user.js
@@ -0,0 +1,55 @@
+﻿user_pref("apz.overscroll.enabled", false); //  отключение оверскрола при планшетном листании
+user_pref("browser.altClickSave", true); //сохранять изображение или ссылку через доп+клик
+user_pref("browser.bookmarks.editDialog.showForNewBookmarks", false); // диалог сохранения закладки
+user_pref("devtools.toolbox.zoomValue", "1.2"); // увеличивает размер текста по умолчанию в инструментах разработчика
+user_pref("browser.cache.disk.enable", false); // не записывать кеш на диск
+user_pref("browser.compactmode.show", true); // компактный режим
+user_pref("browser.ctrlTab.sortByRecentlyUsed", true); // переходить на последнюю вкладку по упр+таб
+user_pref("browser.fixup.alternate.suffix", ".ru"); // окончание ссылки
+user_pref("browser.search.hiddenOneOffs", "Google,Википедия (ru),DuckDuckGo"); // убрать значки поисковых систем из адресной строки
+user_pref("browser.sessionhistory.max_total_viewers", 0); // не сохранять сессии (?)
+user_pref("browser.sessionstore.interval", 120000); // периодчиность автосохранения сессии (2 минуты)
+user_pref("browser.tabs.closeWindowWithLastTab", false); // не закрывать браузер при закрытии последней вкладки
+user_pref("browser.tabs.tabMinWidth", 50); // минимальная ширина вкладки в сжатом состоянии
+user_pref("browser.tabs.insertAfterCurrent", true); // добавлять новую вкладку после текущей
+user_pref("browser.tabs.loadBookmarksInBackground", true); // при открытии закладок оставаться на исходной открытой вкладке до завершения и открываться только при нажатии на вкладку
+user_pref("browser.tabs.loadBookmarksInTabs", true); //открывать закладки в новых вкладках
+user_pref("browser.tabs.loadDivertedInBackground", true); // открывать вкладку в фоне
+user_pref("browser.tabs.min_inactive_duration_before_unload", 30000); // частота обновления (?)
+user_pref("browser.tabs.tabClipWidth", 999); // отображать кнопку закрытия (крестик) только на активной вкладке
+user_pref("browser.uidensity", 1); // компактный режим
+user_pref("browser.urlbar.filter.javascript", false);
+user_pref("browser.urlbar.showSearchSuggestionsFirst", false); // показывать в предложениях сначала закладки
+user_pref("browser.urlbar.suggest.searches", false); // показывать в предложениях сначала закладки
+user_pref("dom.webnotifications.enabled", false); // уведомления сайтов (?)
+user_pref("extensions.htmlaboutaddons.recommendations.enabled", false); // рекомендации сайтов (?)
+user_pref("font.name.monospace.x-cyrillic", "MxPlus IBM VGA 8x12"); // моноширный шрифт по-умолчанию
+user_pref("font.name.monospace.x-western", "MxPlus IBM VGA 8x12"); // моноширный шрифт по-умолчанию
+user_pref("general.smoothScroll.msdPhysics.enabled", true); // притормаживать в конце скролла
+user_pref("layout.word_select.eat_space_to_next_word", false); // не выделять пробел после слова
+user_pref("mousewheel.default.delta_multiplier_y", 150); // ускорить скролл
+user_pref("widget.non-native-theme.scrollbar.style", 2); // тонкая полоса прокрутки
+user_pref("image.http.accept", "*/*"); // убрать webp как стандартные изображения
+
+// user_pref("browser.download.dir", "F:\\Архив\\Загрузки");
+// user_pref("browser.download.folderList", 2);
+// user_pref("browser.download.lastDir", "C:\\Users\\ivleonov\\Desktop");
+// user_pref("browser.download.panel.shown", true);
+// user_pref("media.block-autoplay-until-in-foreground", true); // не запускать видео на вкладке, пока не открыта
+// user_pref("browser.cache.disk.enable", false);  // использования кеша памяти вместо кеша диска (проверить)
+// user_pref("browser.cache.memory.enable", true); 1 //использования кеша памяти вместо кеша диска (проверить)
+// user_pref("browser.urlbar.suggest... ", true); // настройки предложений
+// user_pref("network.http.rcwn.enabled", false);  // 
+// user_pref("network.buffer.cache.size", 262144); // сетевое ускорение 
+// user_pref("network.buffer.cache.count", 128); // сетевое ускорение
+// user_pref("network.http.max-connections", 1800); // сетевое ускорение
+// user_pref("network.http.max-connections-per-server", 32); //  сетевое ускорение
+// user_pref("network.http.max-persistent-connections-per-server", 12); // сетевое ускорение
+// user_pref("network.http.max-urgent-start-excessive-connections-per-host", 6);  // сетевое ускорение
+// user_pref("network.http.pacing.requests.burst", 16); // сетевое ускорение
+// user_pref("network.http.pacing.requests.min-parallelism", 8); // сетевое ускорение
+// user_pref("network.websocket.max-connections", 256); // сетевое ускорение
+// user_pref("network.ssl_tokens_cache_capacity", 32768); // сетевое ускорение 
+// user_pref("gfx.font_rendering.cleartype_params.rendering_mode", 5); // настройка сглаживания шрифтов как они задуманы
+// user_pref("browser.tabs.loadBookmarksInTabs", true); // открывать адрес в новой вкладке:
+// user_pref("layout.css.visited_links_enabled", false);  // не отображать посещенные ссылки
